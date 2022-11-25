aee1f00dca18f6901bba54b1ffd28eb77791ac13
smagdela <smagdela@student.42.fr>
2022-11-25 15:11:58 +0100

    mariadb seems done for the most part. Moving on to wordpress.

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -4,12 +4,19 @@ service mysql start
 
 mysql_secure_installation << STOP
 
-n
-n
+Y
+$SQL_ROOT_PWD
+$SQL_ROOT_PWD
 Y
 Y
 Y
 Y
 STOP
 
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+mysql -e "CREATE USER IF NOT EXISTS mysql@localhost IDENTIFIED BY $SQL_USER_PWD"
+mysql -e "FLUSH PRIVILEGES"
 
+service mysql stop
+
+exec mysqld_safe
