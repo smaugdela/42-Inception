7933c5b6aff1ac2a80eed3ad010b93747c0e5f53
smagdela <smagdela@student.42.fr>
2022-11-29 17:36:46 +0100

    mariadb is fully functionnal. Now working on making nginx and wordpress communicate.

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,21 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
 service mysql start
 
-mysql_secure_installation << STOP
+expect -c "
 
-Y
-$SQL_ROOT_PWD
-$SQL_ROOT_PWD
-Y
-Y
-Y
-Y
-STOP
+set timeout 10
 
-mysql -e "CREATE DATABASE IF NOT EXISTS '$DB_NAME';"
-mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@localhost IDENTIFIED BY '$SQL_USER_PWD';"
-mysql -e "GRANT ALL PRIVILEGES ON '$DB_NAME' TO '$SQL_USER' IDENTIFIED BY '$SQL_USER_PWD';"
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none):\"
+send \"\r\"
+
+expect \"Change the root password?\"
+send \"Y\r\"
+
+expect \"New password:\"
+send \"$SQL_ROOT_PWD\r\"
+
+expect \"Re-enter new password:\"
+send \"$SQL_ROOT_PWD\r\"
+
+expect \"Remove anonymous users?\"
+send \"Y\r\"
+
+expect \"Disallow root login remotely?\"
+send \"Y\r\"
+
+expect \"Remove test database and access to it?\"
+send \"Y\r\"
+
+expect \"Reload privilege tables now?\"
+send \"Y\r\"
+
+expect eof
+"
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_USER_PWD';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$SQL_USER' IDENTIFIED BY '$SQL_USER_PWD';"
 mysql -e "FLUSH PRIVILEGES;"
 
 service mysql stop
