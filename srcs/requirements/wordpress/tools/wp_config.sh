57d9922ba0928b2d728dfd3ef4cd479140f675bb
smagdela <smagdela@student.42.fr>
2022-12-07 19:54:26 +0100

    same

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/html
+#cd /var/www/html
 
 #curl -O https://wordpress.org/wordpress-6.1.tar.gz
 #tar -xzvf wordpress-6.1.tar.gz
@@ -10,14 +10,17 @@ cd /var/www/html
 
 #cp wp-config.php wp-config-sample.php
 
-sed -i "s/username_here/$SQL_USER/g" wp-config.php
-sed -i "s/password_here/$SQL_USER_PWD/g" wp-config.php
-sed -i "s/localhost/$DB_HOST/g" wp-config.php
-sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
+#sed -i "s/username_here/$SQL_USER/g" wp-config.php
+#sed -i "s/password_here/$SQL_USER_PWD/g" wp-config.php
+#sed -i "s/localhost/$DB_HOST/g" wp-config.php
+#sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
 
 #mv wp-config-sample.php wp-config.php
 
-sleep 15
+rm /var/www/html/wp-config.php
+#wp config create --path="/var/www/html/" --dbname=$DB_NAME --dbuser=$SQL_USER --dbpass=$SQL_USER_PWD --dbhost=$DB_HOST --allow-root --skip-check
+
+sleep 20
 
 echo "Launching php-fpm..."
 
