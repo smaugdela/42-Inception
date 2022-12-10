c77869aedc59373f61179eed3a10c93fd94222e7
smagdela <smagdela@student.42.fr>
2022-12-10 21:50:33 +0100

    Everything is working fine, now finishing with the Makefile

--- srcs/requirements/wordpress/tools/wp_startup.sh
+++ srcs/requirements/wordpress/tools/wp_startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+sleep 15
+
 wp config create --path="/var/www/html/" --dbname=$DB_NAME --dbuser=$SQL_USER --dbpass=$SQL_USER_PWD --dbhost=$DB_HOST --allow-root --skip-check
 
-# sleep 15
+wp core install --url=$HOSTNAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$MAIL --allow-root
 
-wp core install --url=$HOSTNAME --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PWD --admin_email=$MAIL
-
-echo "wordpress configured and ready!"
+echo "Wordpress is configured and ready!"
 
 exec /usr/sbin/php-fpm7.3 -F
